8cb5c71c4f1361f36bb30de9c27377603fcb6325
CondoritoBR <rflramos@gmail.com>
2018-08-24 09:25:18 -0300

    Inserted method CRUD int ProdutoDAL class

--- LojaSite/DAL/ProdutoDAL.cs
+++ LojaSite/DAL/ProdutoDAL.cs
@@ -20,5 +20,55 @@ namespace LojaSite.DAL
 
             return lista;
         }
+
+        public Produto Consultar(int id)
+        {
+            //criar classe de contexto
+            LojaContext context = new LojaContext();
+
+            //recuperar o o bjeto produro de um determinado id
+            Produto produto = context.Produto.Find(id);
+
+            return produto;
+        }
+
+        public void Inserir(Produto produto)
+        {
+            //classe de contexto
+            LojaContext context = new LojaContext();
+
+            //adicionando um objeto produto
+            context.Produto.Add(produto);
+
+            //salvando as alterações no banco
+            context.SaveChanges();
+
+        }
+
+        public void Alterar (Produto produto)
+        {
+            //instanciando a classe de contexto
+            LojaContext context = new LojaContext();
+
+            //Informando a classe de contexo o status do produto MODIFIED
+            context.Entry(produto).State = System.Data.Entity.EntityState.Modified;
+
+            //salvando as alterações
+            context.SaveChanges();
+        }
+
+        public void Excluir (int id)
+        {
+            //Instanciando a classe contexto
+            LojaContext context = new LojaContext();
+
+            //Efetuando busca por um ID
+            Produto produto = context.Produto.Find(id);
+
+            //Informa ao contexto que um objeto foi deletado
+            context.Entry(produto).State = System.Data.Entity.EntityState.Deleted;
+
+            context.SaveChanges();
+        }
     }
 }
